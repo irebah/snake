b8873e8780338dc94f62c96c9e18963c45775e5b
Ismail Rebah <ismail.rebah@gmail.com>
2024-10-24 21:15:45 +1300

    feat: adding the game area and the info area

---
+++ src/App.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  test("it should render", () => {
+    render(<App />);
+  });
+
+  test("it should render the title", () => {
+    render(<App />);
+
+    expect(screen.getByText("Snake")).toBeInTheDocument();
+  });
+
+  test("it should render the game area and the info area", () => {
+    render(<App />);
+
+    expect(screen.getByTestId("info-area")).toBeInTheDocument();
+    expect(screen.getByTestId("game-area")).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 const App = () => {
-  return <span className="text-red-400 text-2xl">hello world</span>;
+  return (
+    <main className="flex-col gap-4 center w-screen h-screen p-5 md:p-10">
+      <p className="text-4xl">Snake</p>
+      <section className="gameArea center flex-col md:flex md:flex-row gap-3 relative">
+        <div
+          data-testid="info-area"
+          className="md:w-[250px] md:block w-full flex md:h-full md:order-last mb-2 md:mb-0 bg-red-400"
+        >
+          info area
+        </div>
+        <div
+          data-testid="game-area"
+          className="flex-1 h-full md:h-100 w-full bg-green-400"
+        >
+          game area
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default App;
