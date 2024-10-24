8489a33c640b9beffe26e317b2612920b0694591
Ismail Rebah <ismail.rebah@gmail.com>
2024-10-24 15:28:28 +1300

    chore: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <span className="text-red-400 text-2xl">hello world</span>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/snake" : "/",
+  plugins: [react()],
+  test: {
+    environment: "happy-dom",
+    setupFiles: "./setupTests.ts",
+    globals: true,
+    outputFile: {
+      junit: "./junit-report.xml",
+    },
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [...coverageConfigDefaults.exclude],
+    },
+  },
+});
